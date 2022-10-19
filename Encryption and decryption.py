5152b9aae6b1429acf1bbb483aef71b0c1f87206
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2022-10-19 08:45:42 +0100

    Update Encryption and decryption.py

--- Encryption and decryption.py
+++ Encryption and decryption.py
@@ -82,3 +82,22 @@ def encrypt(text,shift):
     print("".join(new))
 
 print(encrypt(text, shift))
+
+##############################################################
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+    ciper_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        ciper_text += new_letter
+    print(ciper_text)
+
+print(encrypt(text, shift))
