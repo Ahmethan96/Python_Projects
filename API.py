0ff722b2f80efced742d8e3f5816b766c2466187
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2023-05-23 21:55:35 +0100

    Update API.py

--- API.py
+++ API.py
@@ -1,1 +1,11 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
+@app.route("/")
+
+def get_user():
+
+    return "Ibrahim Ahmethan"
+
+if __name__ == "__main__":
+    app.run(debug=True)
