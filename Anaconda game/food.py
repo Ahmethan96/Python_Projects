20cd4338a82fa70edf1fa323199b519639dbc17e
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2023-06-10 20:04:56 +0100

    Add files via upload

---
+++ Anaconda game/food.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len= 0.5, stretch_wid= 0.5)
+        self.color("blue")
+        self.speed("fastest")
+        random_x = random.randint(-200, 200)
+        random_y = random.randint(-200, 200)
+        self.goto(random_x, random_y)
+
+
+    def refreash(self):
+        random_x = random.randint(-200, 200)
+        random_y = random.randint(-200, 200)
+        self.goto(random_x, random_y)
+
+
+
+
+
+
+
+
+
+
+
+food = Food()
+print(food.shape("circle"))
