018ae36b9b90ef4eec78782db36fe54f3710d490
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2023-06-14 11:03:34 +0100

    Add files via upload

--- Anaconda game/snake.py
+++ Anaconda game/snake.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 screen = Screen()
+
 class Snake:
 
     def __init__(self):
@@ -7,25 +8,43 @@ class Snake:
         self.segments = []
         self.structure()
 
+
     def structure(self):
-        self.ibo = 1996
+
         for positions in self.starting_position:
+            self.add_segment(positions)
             # print(i[0], i[1])
 
-            new_segment = Turtle("square")
-            new_segment.penup()
-            new_segment.color("white")
-            new_segment.goto(positions)
-            self.segments.append(new_segment)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.penup()
+        new_segment.color("white")
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+
+
+    def extend(self):
+        ## add a new segment to the snake.
+        self.add_segment(self.segments[-1].position())
+
+
+
 
     def move(self):
         # self.structure()
+
         print(len(self.segments))
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(20)
+
+
+
 
     def up(self):
         self.segments[0].setheading(90)
