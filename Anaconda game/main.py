27c9bd24b66915592bda855220cf9ec3fbb5a9d4
ibrahim ahmethan <46056890+Ahmethan96@users.noreply.github.com>
2023-06-14 11:02:48 +0100

    Add files via upload

--- Anaconda game/main.py
+++ Anaconda game/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 from snake import Snake
 import time
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width= 600, height= 600)
@@ -9,7 +11,10 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 screen.listen()
+score = Scoreboard()
+
 
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -18,8 +23,30 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
+
+
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.segments[0].distance(food) < 15:
+        food.refreash()
+        snake.extend()
+        score.incrase_score()
+
+    if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280 :
+        score.game_over()
+        game_is_on = False
+
+    for segment in snake.segments:
+        if segment == snake.segments[0]:
+            pass
+        elif snake.segments[0].distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
+
+
+
 screen.exitonclick()
 
